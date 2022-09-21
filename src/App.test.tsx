09e6d13a62b63bbfa3bad2cf462b2fba33d726a8
Wil89 <wilber@fonoma.com>
2022-09-21 17:59:41 +0200

    tdd initial scoreboard render state

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import ScoreBoard from "./components/ScoreBoard";
 
-test("renders the score board presence", () => {
-  render(<App />);
-  expect(
-    screen.getByText(/Football World Cup Score Board/i)
-  ).toBeInTheDocument();
+describe("test scoreboard", () => {
+  test("renders the score board presence in App", () => {
+    render(<App />);
+    expect(
+      screen.getByText(/Football World Cup Score Board/i)
+    ).toBeInTheDocument();
+    expect(screen.getByText(/Start Game/i)).toBeInTheDocument();
+    expect(screen.getByText(/Finish Game/i)).toBeInTheDocument();
+    expect(screen.getByText(/Update Score/i)).toBeInTheDocument();
+    expect(screen.getByText(/Get Summary/i)).toBeInTheDocument();
+    expect(screen.getByText(/Matches/i)).toBeInTheDocument();
+  });
+
+  test("scoreboard initial empty state", () => {
+    render(<ScoreBoard />);
+    expect(screen.getByRole('list')).toBeInTheDocument();
+    const items = screen.queryAllByRole('listitem');
+    expect(items).toHaveLength(0);
+  });
 });
--- src/components/ScoreBoard.tsx
+++ src/components/ScoreBoard.tsx
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
-type Props = {};
+enum GameStatus {
+  Started,
+  Finished,
+}
+interface Game {
+  id: string;
+  status: GameStatus;
+  homeTeam: string;
+  awayTeam: string;
+  scoreHome: number;
+  scoreAway: number;
+  finishGame: () => void;
+  updateGame: (scoreHome: number, scoreAway: number) => void;
+}
 
-const ScoreBoard = (props: Props) => {
-  return <div>
-    <h1>Football World Cup Score Board</h1>
-    </div>;
+const ScoreBoard = () => {
+  return (
+    <div>
+      <h1>Football World Cup Score Board</h1>
+      <button>Start Game</button>
+      <button>Finish Game</button>
+      <button>Update Score</button>
+      <button>Get Summary</button>
+
+      <div>
+        <h2>Matches</h2>
+        <ul></ul>
+      </div>
+    </div>
+  );
 };
 
 export default ScoreBoard;
